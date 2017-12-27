1125267eecb1815a39b6b4d08b6b6a4bc4973335
Seyam <seyam.bd.net@gmail.com>
2017-12-27 15:37:34 +0600

    Update edisonOnOffListener.js

--- edisonOnOffListener.js
+++ edisonOnOffListener.js
@@ -54,6 +54,10 @@ broker.on('connect', function () {
 
 
 broker.on('message', function (topic, message) {
+  
+  console.log(topic.toString());
+  console.log(message.toString());
+  
   if(topic=="light"){
     if(message == 'on'){
       led.write(1);
